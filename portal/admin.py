d8589fd5c39806d6dcb92dc3744fb57185a1083a
Sahil Ansari <sahilansari1107@gmail.com>
2022-07-29 20:37:15 +0530

    initial commit

---
+++ portal/admin.py
@@ -0,0 +1,48 @@
+from django.contrib import admin
+from .models import Class, Subject, Chapter, Note, Timing, Students, Dept, Teacher
+# Register your models here.
+
+@admin.register(Class)
+class ClassAdmin(admin.ModelAdmin):
+    list_display=['class_name','description']
+
+
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display=['class_name','subject_name','description']    
+    
+
+@admin.register(Chapter)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display=['class_name','subject_name','chapter_name']   
+    search_fields=['chapter_name']
+    
+
+
+@admin.register(Note)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display=['chapter_name','description']
+    search_fields=['chapter_name']
+       
+
+@admin.register(Timing)
+class TimingAdmin(admin.ModelAdmin):
+    list_display=['date','time']       
+
+@admin.register(Students)
+class UserPictureAdmin(admin.ModelAdmin):
+    list_display=['user','phone']
+
+class DeptAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id')
+    search_fields = ('name', 'id')
+    ordering = ['name']
+admin.site.register(Dept, DeptAdmin)
+
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('name', 'dept')
+    search_fields = ('name', 'dept__name')
+    ordering = ['dept__name', 'name']
+admin.site.register(Teacher, TeacherAdmin)    
